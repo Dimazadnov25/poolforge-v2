e5160761ee538d28375192bdb1384439ec2da998
Dimazadnov25 <dmitrij25@gmx.de>
2026-04-23 20:22:59 +0200

    Add RPC fallback

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { useMemo } from 'react'
 import PoolDashboard from './components/PoolDashboard'
 import '@solana/wallet-adapter-react-ui/styles.css'
 
-const RPC = 'import.meta.env.VITE_RPC_URL'
+const RPC = 'import.meta.env.VITE_RPC_URL || 'https://mainnet.helius-rpc.com/?api-key=e129ef66-4dde-4f0d-bc1b-e4197604806d''
 
 export default function App() {
   const wallets = useMemo(() => [new PhantomWalletAdapter()], [])
