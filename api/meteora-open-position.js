690c5230a0089b559df653400b53982b0ec73d85
Dimazadnov25 <dmitrij25@gmx.de>
2026-04-27 23:43:13 +0200

    Use CommonJS for Meteora SDK

--- api/meteora-open-position.js
+++ api/meteora-open-position.js
@@ -1,48 +1,35 @@
-import DLMM from "@meteora-ag/dlmm";
-import { Connection, PublicKey, Keypair } from "@solana/web3.js";
-import BN from "bn.js";
+const DLMM = require("@meteora-ag/dlmm").default;
+const { Connection, PublicKey, Keypair } = require("@solana/web3.js");
+const BN = require("bn.js");
 
 const POOL_ADDRESS = "5rCf1DM8LjKTw4YqhnoLcngyZYeNnQqztScTogYHAS6";
 const BIN_SPREAD = 10;
-const SOL_AMOUNT = 0.05 * 1e9; // 0.05 SOL in lamports
+const SOL_AMOUNT = 0.05 * 1e9;
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   try {
     const RPC = process.env.VITE_RPC_URL;
     const connection = new Connection(RPC, "confirmed");
-
     const userPubkey = new PublicKey("BFU5gQ5jYq534vSDKGnBSNffwtoTZFkeo68WJmviVVzj");
     const poolPubkey = new PublicKey(POOL_ADDRESS);
 
-    // Pool laden
     const dlmmPool = await DLMM.create(connection, poolPubkey);
-
-    // Aktiven Bin holen
     const activeBin = await dlmmPool.getActiveBin();
     const minBinId = activeBin.binId - BIN_SPREAD;
     const maxBinId = activeBin.binId + BIN_SPREAD;
 
-    // Neues Position Keypair
     const positionKeypair = Keypair.generate();
-
-    // SOL Menge
     const totalXAmount = new BN(SOL_AMOUNT);
     const totalYAmount = new BN(0);
 
-    // Transaktion bauen
     const createPositionTx = await dlmmPool.initializePositionAndAddLiquidityByStrategy({
       positionPubKey: positionKeypair.publicKey,
       user: userPubkey,
       totalXAmount,
       totalYAmount,
-      strategy: {
-        maxBinId,
-        minBinId,
-        strategyType: 0, // Spot
-      },
+      strategy: { maxBinId, minBinId, strategyType: 0 },
     });
 
-    // Transaktion serialisieren für Phantom
     createPositionTx.recentBlockhash = (await connection.getLatestBlockhash()).blockhash;
     createPositionTx.feePayer = userPubkey;
     createPositionTx.partialSign(positionKeypair);
