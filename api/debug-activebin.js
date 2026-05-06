0e72b51f5d071dabeaddae5b45a8550d9f6bc17c
Dimazadnov25 <dmitrij25@gmx.de>
2026-05-06 15:22:06 +0200

    cleanup: remove unused Meteora + debug API routes

--- api/debug-activebin.js
+++
@@ -1,9 +0,0 @@
-import { Connection, PublicKey } from "@solana/web3.js";
-const POOL = "5rCf1DM8LjKTw4YqhnoLcngyZYeNnQqztScTogYHAS6";
-export default async function handler(req, res) {
-  const connection = new Connection(process.env.VITE_RPC_URL, "confirmed");
-  const info = await connection.getAccountInfo(new PublicKey(POOL));
-  const offsets = {};
-  for(let i=60;i<=100;i+=4) offsets[i]=info.data.readInt32LE(i);
-  return res.status(200).json({ offsets });
-}
